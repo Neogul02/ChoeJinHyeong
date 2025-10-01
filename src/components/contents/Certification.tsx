b7ffd218e677dfb53e7b139f43271d2b71533012
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2025-10-01 16:34:30 +0900

    feat: Add Certification section
    Introduced a new Certification component and integrated it into the main page and section navigation. Updated Activities content for clarity and accuracy.

---
+++ src/components/contents/Certification.tsx
@@ -0,0 +1,20 @@
+// 기술 스택
+export default function Certification() {
+  return (
+    <section className='mb-20'>
+
+      {/* 777 */}
+      <div className='flex flex-row justify-between my-10 ml-10'>
+        <div className='w-1/2 pr-6'>
+          <div className='text-[22px] font-bold'>정보처리기사</div>
+          <div className='text-[16px] font-bold ml-3'>
+            <div>2025.09.12</div>
+            <div className='font-light'>한국산업인력공단</div>
+            
+          </div>
+        </div>
+        
+      </div>
+    </section>
+  )
+}
